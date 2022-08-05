5bed48b240968bc5d2e8d56c0e99171137959a07
Hamza-Ansari-9690 <11as1827000599@gmail.com>
2022-08-05 17:38:46 +0530

    Adding HAHAHA in practice file

--- Practice.java
+++ Practice.java
@@ -3,6 +3,7 @@ class Practice {
 
         System.out.println("Hello Java");
         System.out.println("How are you");
+        System.out.println("HAHAHAHAHAH");
         System.out.println("I am fine java");
     }
 }
