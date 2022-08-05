e39426767d84df781b7531ac20b45eca8142e98e
Hamza-Ansari-9690 <11as1827000599@gmail.com>
2022-08-05 14:53:35 +0530

    Adding Third Parameter from code

--- Addition.java
+++ Addition.java
@@ -3,6 +3,7 @@ public class Addition {
     public static void main(String[] args) {
         int a = 9;
         int b = 5;
-        System.out.println(a + b);
+        int C = 3;
+        System.out.println(a + b + C);
     }
 }
