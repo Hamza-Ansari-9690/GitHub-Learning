b32f2ebe90c97852e88d1a204bef42c6b2a43435
Hamza-Ansari-9690 <110654670+Hamza-Ansari-9690@users.noreply.github.com>
2022-08-05 17:37:15 +0530

    Adding one more line in practice file

--- Practice.java
+++ Practice.java
@@ -3,6 +3,7 @@ class Practice {
 
         System.out.println("Hello Java");
         System.out.println("How are you");
+         System.out.println("How are you 2");
         System.out.println("I am fine java");
     }
 }
