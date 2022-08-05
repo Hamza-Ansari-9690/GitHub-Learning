b9ade54cb0f11980e0bf9f18be1c41154c6280ae
Hamza-Ansari-9690 <110654670+Hamza-Ansari-9690@users.noreply.github.com>
2022-08-05 14:52:12 +0530

    Add third parameter

--- Addition.java
+++ Addition.java
@@ -3,6 +3,7 @@ public class Addition {
     public static void main(String[] args) {
         int a = 9;
         int b = 5;
-        System.out.println(a + b);
+        int c = 9;
+        System.out.println(a + b + c);
     }
 }
